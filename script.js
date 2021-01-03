fb0d13920840efb7d816cfeaf8b90803af5ff902
LuzianeFreitas <freitas.lu@outlook.com>
2021-01-02 21:30:16 -0300

    Created snake

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 let canvas = document.getElementById('snake');
 let context = canvas.getContext('2d'); // renderiza o desenho dentro do canvas
 let box = 32;
+let snake = [];
+snake[0] = {
+    x: 8 * box,
+    y: 8 * box
+}
 
 // desenha o canvas
 function criarBG() {
@@ -8,4 +13,12 @@ function criarBG() {
     context.fillRect(0, 0, 16 *box, 16 * box);
 }
 
+function criarCobrinha() {
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = 'green';
+        context.fillRect(snake[i].x, snake[i].y, box, box);
+    }
+}
+
 criarBG();
+criarCobrinha();
