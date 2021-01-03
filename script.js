11cddd042192c3cb2e743cc436321fe0eabbf94b
LuzianeFreitas <freitas.lu@outlook.com>
2021-01-02 22:22:26 -0300

    Created game over

--- script.js
+++ script.js
@@ -43,10 +43,19 @@ function update(event) {
 }
 
 function iniciaJogo() {
+
     if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
     if (snake[0].x < 0 * box && direction == 'left') snake[0].x = 16 * box;
     if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
     if (snake[0].y < 0 * box && direction == 'up') snake[0].y = 16 * box;
+
+
+    for( i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
 
     criarBG();
     drawFood();
