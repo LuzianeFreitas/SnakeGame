ec001e10d7adc8a4d3c57c493763734a7bdc1766
LuzianeFreitas <freitas.lu@outlook.com>
2021-01-02 22:00:03 -0300

    Set snake directions

--- script.js
+++ script.js
@@ -23,7 +23,22 @@ function criarCobrinha() {
     }
 }
 
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if (event.keyCode == 37 && direction != 'right') direction = 'left';
+    if (event.keyCode == 38 && direction != 'down') direction = 'up';
+    if (event.keyCode == 39 && direction != 'left') direction = 'right';
+    if (event.keyCode == 40 && direction != 'up') direction = 'down';
+
+}
+
 function iniciaJogo() {
+    if (snake[0].x > 15 * box && direction == 'right') snake[0].x = 0;
+    if (snake[0].x < 0 * box && direction == 'left') snake[0].x = 16 * box;
+    if (snake[0].y > 15 * box && direction == 'down') snake[0].y = 0;
+    if (snake[0].y < 0 * box && direction == 'up') snake[0].y = 16 * box;
+
     criarBG();
     criarCobrinha();
 
@@ -43,8 +58,9 @@ function iniciaJogo() {
     }
 
     snake.unshift(newHead);
-    
+
 }
 
 let jogo = setInterval(iniciaJogo, 100);
+
 
